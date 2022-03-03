2273228b1225bab8d642ee2352b354ffdeee89e6
alibhtty <77213825+alibhtty@users.noreply.github.com>
2022-03-03 03:39:33 +0100

    2.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,11 +79,11 @@ function scrollActive(){
               sectionTop = current.offsetTop - 58,
               sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        /* if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
         }else{
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
+        } */
     })
 }
 window.addEventListener('scroll', scrollActive)
